1ef3da9842511bcaa0710349db10cbd0309ba61a
inarizenko <enjolrasacosta@gmail.com>
2025-10-05 06:59:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const actualVersion = 'Beta 0.1';
 const leftMenuManual = '<aside><h>Lore</h><ul><li><b data-href="/aetherra/manual/history.html">Historia</b></li><li><b data-href="/aetherra/manual/religion.html">Religión</b></li></aside>';
 
 $(function(){
-  $('body').prepend('<nav><a href="/aetherra/index.html">Aetherra</a><span><a href="/aetherra/manual/">Manual</a><a href="/aetherra/about.html">Acerca de Aetherra</a><a href="/aetherra/credits.html">Créditos</a></span></nav><header><h1>Aetherra</h1></header>');
+  var header = '<header><h1>Aetherra</h1></header>';
   if (window.location.pathname.indexOf('/manual') !== -1) {
     $('body').addClass('manual');
+    header = '<header><h1>Manual del Jugador</h1></header>';
   }
+  $('body').prepend('<nav><a href="/aetherra/index.html">Aetherra</a><span><a href="/aetherra/manual/">Manual</a><a href="/aetherra/about.html">Acerca de Aetherra</a><a href="/aetherra/credits.html">Créditos</a></span></nav>'+header);
   $('body.manual').prepend(leftMenuManual);
     // Click handler for the fake links
   $('aside b[data-href]').on('click', function() {
