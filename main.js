2ebbc4453f87bc6f4052d0abc1e7c779ebf42089
inarizenko <enjolrasacosta@gmail.com>
2025-10-05 04:02:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const actualVersion = 'Beta 0.1';
+
 $(function(){
-  const actualVersion = 'Beta 0.1';
   $('body').prepend('<nav><a href="/aetherra/index.html">Aetherra</a><span><a href="/aetherra/manual/">Manual</a><a href="/aetherra/about.html">Acerca de Aetherra</a><a href="/aetherra/credits.html">Créditos</a></span></nav><header><h1>Aetherra</h1></header>');
 });
 
