7a5cf7eaf1d27986874d7a7cef433e29ccb507ec
inarizenko <enjolrasacosta@gmail.com>
2025-10-05 03:12:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('body').prepend('<nav><a href="/aetherra/index.html">Aetherra</a><span><a href="/aetherra/manual/">Manual</a><a href="/aetherra/about.html">Acerca de Aetherra</a><a href="/aetherra/license.md">Licencia</a></span></nav><header><h1>Aetherra</h1></header>');
+  $('body').prepend('<nav><a href="/aetherra/index.html">Aetherra</a><span><a href="/aetherra/manual/">Manual</a><a href="/aetherra/about.html">Acerca de Aetherra</a><a href="/aetherra/credits.html">Créditos</a></span></nav><header><h1>Aetherra</h1></header>');
 });
 
 $(window).on('load',function(){
