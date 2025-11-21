31f18a4a837da27e109e190d385ed546559d1791
inarizenko <enjolrasacosta@gmail.com>
2025-11-21 05:54:37 +0100

    Update fantasmagoria.js

--- shops/thareen/zhaerith/fantasmagoria.js
+++ shops/thareen/zhaerith/fantasmagoria.js
@@ -210,7 +210,7 @@ document.addEventListener("DOMContentLoaded", () => {
           </div>
           <div class="tags"><span class="tag">${it.type}</span><span class="tag">${it.rarity}</span><span class="tag">${it.attunement}</span></div>
           <div class="actions">
-            <a href="${it.5etools}" title="Ver página de item en 5e.tools"><img src="https://i.imgur.com/Ym2ZJlQ.png"/></a>
+            <a href="${it.tools}" title="Ver página de item en 5e.tools"><img src="https://i.imgur.com/Ym2ZJlQ.png"/></a>
             <button class="btn-ghost" data-id="${it.id}" aria-label="Ver detalles">Detalles</button>
             <button class="btn" data-buy="${it.id}">Añadir</button>
           </div>
@@ -231,7 +231,7 @@ document.addEventListener("DOMContentLoaded", () => {
     $("#modalName").textContent = it.name;
     $("#modalDesc").textContent = it.desc;
     $("#modalPrice").textContent = `${it.price} PO`;
-    $("#modalAdd+a").setAttribute("title", it.5etools);
+    $("#modalAdd+a").setAttribute("title", it.tools);
     $("#modalAdd").onclick = () => {
       addToCart(it.id);
       closeModal();
